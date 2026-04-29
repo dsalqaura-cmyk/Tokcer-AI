b7f9d5fdff58ba12912b04ca2c0ee37a6658c0d2
dsalqaura-cmyk <dsalqaura-cmyk@users.noreply.github.com>
2026-04-30 04:50:28 +0700

    fix: restore centered layout for partner dashboard and add missing payment_method column

--- src/pages/PartnerDashboard.jsx
+++ src/pages/PartnerDashboard.jsx
@@ -325,7 +325,7 @@ const PartnerDashboard = () => {
         toggleLang={toggleLang} 
         handleLogout={handleLogout} 
       />
-      <div className="flex-1 flex flex-col min-h-screen lg:ml-64">
+      <div className="flex-1 flex flex-col min-h-screen">
         <PartnerHeader 
           t={t} 
           partnerData={partnerData} 
